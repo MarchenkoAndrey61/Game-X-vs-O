7fb64db3731165b1a394e047504563ff6b72571f
MarchenkoAndrey61 <xayc755@yandex.ru>
2019-12-09 16:37:07 +0300

    complete game

--- src/Board.js
+++ src/Board.js
@@ -6,38 +6,18 @@ import Square from './Square'
 
 
 class Board extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-
-  handleClick(i){
-    const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-        squares:squares,
-        xIsNext: !this.state.xIsNext,    
-    });
-  }
-
   renderSquare(i) {
-    return (  
-     <Square 
-            value = {this.state.squares[i]}
-            onClick = { () => this.handleClick(i) }        
-     />
-    );  
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -57,5 +37,8 @@ class Board extends React.Component {
     );
   }
 }
+
+
+
 
 export default Board
